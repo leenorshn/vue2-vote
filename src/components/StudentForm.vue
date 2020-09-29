5302d18ad5c9ccd5eed2d8c55240cd1e32d909cd
leenorshn <katembovictor@gmail.com>
2020-09-29 20:47:04 +0100

    modernite

--- src/components/StudentForm.vue
+++ src/components/StudentForm.vue
@@ -1,24 +1,74 @@
 <template>
-    <div class="container">
-        {{name}}
+  <div class="student-form-main">
+    <input type="text" placeholder="Noms completes" />
+    <div class="combo">
+      <label for="pet-select">faculte:</label>
+      <select name="pets" id="faculte">
+        <option value="GI">Gestion-Info</option>
+        <option value="CU">Communication</option>
+        <option value="SA">Genie-info</option>
+      </select>
     </div>
+    <div class="combo">
+      <label for="pet-select">promotion:</label>
+      <select name="pets" id="promotion">
+        <option value="Prepa">Prepas</option>
+        <option value="G1">G1</option>
+        <option value="G2">G2</option>
+        <option value="G3">G3</option>
+        <option value="L1">L1</option>
+        <option value="L2">L3</option>
+      </select>
+    </div>
+    <input type="text" placeholder="Matricule" />
+    <button>Enregister</button>
+  </div>
 </template>
 <script>
-    export default {
-        data(){
-            return {
-                name:"Jeux de la vie"
-            }
-        },
-        methods:{
+export default {
+  name: "StudentForm",
 
-        }
-    }
+  methods: {},
+};
 </script>
 <style scoped>
-    .container{
-        background:#0e0e0e;
-       
-        color: white;
-    }
+.student-form-main {
+  margin: 10px 10px;
+  padding: 10px;
+
+  background: #e6e1e1;
+  display: flex;
+  color: rgb(12, 11, 11);
+}
+.combo {
+  display: flex;
+  flex-direction: column;
+  margin: 0px 10px;
+}
+#promotion {
+  padding: 6px;
+  width:100px;
+  border:none;
+  border-radius:4px;
+}
+#faculte {
+  padding: 6px;
+  width:100px;
+  border:none;
+  border-radius:4px;
+}
+input {
+  padding: 8px;
+  font-size: 16px;
+  border:none;
+  border-radius:4px;
+}
+button {
+  width: 140px;
+  margin-left: 16px;
+  background-color: #21ce99;
+  color:white;
+  border:none;
+  border-radius:4px;
+}
 </style>
--- src/views/ElecteurPage.vue
+++ src/views/ElecteurPage.vue
@@ -1,19 +1,81 @@
 <template>
     <div class="electeur-main-page">
-        <h4>Electeur</h4>
+        <StudentForm/>
+        <div class="electeur-all-page">
+            <div class="list-electeur">
+
+            </div>
+            <div class="detail-electeur">
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import StudentForm from "../components/StudentForm.vue";
 export default {
     name:"ElecteurPage",
+    components:{
+        StudentForm
+    },
     data() {
-        return {}
-    }
+    return {
+      name: "Jeux de la vie",
+      electeurs:[
+          {
+          name:"Olga Maleki",
+          matricule:"2020-OL-GI-03",
+          promotion:"G3",
+          faculte:"GI"
+      },
+      {
+          name:"Dany molo",
+          matricule:"2020-OL-GI-03",
+          promotion:"G3",
+          faculte:"GI"
+      },
+      {
+          name:"Providence Kazi",
+          matricule:"2020-OL-GI-03",
+          promotion:"G3",
+          faculte:"GI"
+      },
+      {
+          name:"Julien Mongala",
+          matricule:"2020-OL-GI-03",
+          promotion:"G3",
+          faculte:"GI"
+      }
+      ]
+    };
+  },
     
 }
 </script>
 <style scoped>
-
+    .electeur-main-page{
+        display: flex;
+        flex-direction: column;
+        background-color: #21ce99;
+    }
+    .electeur-all-page{
+        display: flex;
+        
+    }
+    .list-electeur{
+        flex:3;
+        background-color:rgb(8, 26, 61);
+        height: 560px;
+        border-radius: 8px;
+    }
+    .detail-electeur{
+        border-radius: 8px;
+        margin-left:10px;
+        margin-right:10px ;
+        flex: 7;
+         background-color:cornflowerblue;
+    }
+    
 </style>
 
